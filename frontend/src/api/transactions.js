69f7fce11cc7a6bc38849f0c985e621789cbad17
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2024-07-25 01:11:28 +0300

    добавил новый мметод для запроса на сервер

--- frontend/src/api/transactions.js
+++ frontend/src/api/transactions.js
@@ -1,53 +1,74 @@
 export const createTransaction = async (transaction) => {
-    try {
-      const response = await fetch('https://localhost:7193/transaction', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(transaction),
-        credentials: 'include',
-      });
-      if (!response.ok) {
-        throw new Error('Failed to create transaction');
-      }
-      return true;
-    } catch (error) {
-      console.error('Error creating transaction:', error);
-      return false;
+  try {
+    const response = await fetch('https://localhost:7193/transaction', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(transaction),
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to create transaction');
     }
-  };
-  
-  export const getTransactions = async () => {
-    try {
-      const response = await fetch('https://localhost:7193/transaction', {
-        method: 'GET',
-        credentials: 'include',
-      });
-      if (!response.ok) {
-        throw new Error('Failed to fetch transactions');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching transactions:', error);
-      return [];
+    return true;
+  } catch (error) {
+    console.error('Error creating transaction:', error);
+    return false;
+  }
+};
+
+
+export const getTransactions = async () => {
+  try {
+    const response = await fetch('https://localhost:7193/transaction', {
+      method: 'GET',
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch transactions');
     }
-  };
-  
-  export const deleteTransaction = async (id) => {
-    try {
-      const response = await fetch(`https://localhost:7193/transaction/${id}`, {
-        method: 'DELETE',
-        credentials: 'include',
-      });
-      if (!response.ok) {
-        throw new Error('Failed to delete transaction');
-      }
-      return true;
-    } catch (error) {
-      console.error('Error deleting transaction:', error);
-      return false;
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    return [];
+  }
+};
+
+export const deleteTransaction = async (id) => {
+  try {
+    const response = await fetch(`https://localhost:7193/transaction/${id}`, {
+      method: 'DELETE',
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete transaction');
     }
-  };
-  
+    return true;
+  } catch (error) {
+    console.error('Error deleting transaction:', error);
+    return false;
+  }
+};
+
+export const filterTransactions = async (filter) => {
+  try {
+    const response = await fetch('https://localhost:7193/transaction/filter', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(filter),
+      credentials: 'include',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch filtered transactions');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching filtered transactions:', error);
+    return [];
+  }
+};
