5bc52d92b1945f33cd948d078e22fcd529091037
Aybek-Sultan04 <sultanov.aybek18@gmail.com>
2024-07-25 01:07:40 +0300

    добавление нового контракта для получения отфильтрованных данных

---
+++ HomeAccounting.Api/Contract/Transactions/GetFilterTransactionRequest.cs
@@ -0,0 +1,8 @@
+﻿using HomeAccounting.Api.Contract.Categories;
+using HomeAccounting.Domain.Enums;
+
+namespace HomeAccounting.Api.Contract.Transactions
+{
+	public record class GetFilterTransactionRequest(DateTime? StartDate,
+		DateTime? EndDate, GetCategoryRequest Category, TransactionType? TransactionType);
+}
